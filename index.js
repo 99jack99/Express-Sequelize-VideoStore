e191e50b4d853d821700108ccc2cc2bf5e36166d
jackson <jackson55adl@gmail.com>
2022-06-09 17:23:57 +0200

    conectando con base de datos

---
+++ index.js
@@ -0,0 +1,20 @@
+
+const express = require('express');
+const app = express();
+const port = 3000;
+
+const db = require('./db/db');
+/* const router = require('./router.js'); */
+
+
+//Middlewares
+
+app.use(express.json());
+/* app.use(router); */
+
+
+db.then(()=>{
+
+    app.listen(port, ()=> {console.log("Server is currently running in port:", port)});
+
+}).catch((err) => console.log(err.message));
