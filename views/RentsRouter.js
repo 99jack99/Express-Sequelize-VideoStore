9717f57216d4be51bbdea1538822055a7ba88568
jackson <jackson55adl@gmail.com>
2022-06-20 10:45:31 +0200

    some deatils, still need some changes

--- views/RentsRouter.js
+++ views/RentsRouter.js
@@ -6,7 +6,7 @@ const isAdmin = require('../middlewares/isAdmin');
 const RentsController = require('../controllers/RentsController');
 
 router.get('/', auth, isAdmin, RentsController.getRents);
-router.post('/rent', auth, isAdmin, RentsController.Rents);
+router.post('/rent', /* auth, */ isAdmin, RentsController.Rents);
 
 //Export
 module.exports = router;
